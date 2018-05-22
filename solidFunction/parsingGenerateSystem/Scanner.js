c4042bf12ead431a6fc0541d4590f244c8a50439
cheekykorkind <cheekorkind@gmail.com>
2018-05-22 22:42:27 +0900

    working

--- solidFunction/parsingGenerateSystem/Scanner.js
+++ solidFunction/parsingGenerateSystem/Scanner.js
@@ -2,9 +2,32 @@
 //     var nodeList = createNode();
 //     return nodeList;
 // );
+
+/*
+    S0 초기상태
+    S0 -> A1 idPattern을 만남.
+    A1 -> A1 idPattern을 만남.
+    A1 -> A2 spacePattern을 만남.
+    A2 -> A2 spacePattern을 만남.
+    A2 -> A3 :을 만남.
+    A3 -> A3 spacePattern을 만남.
+    A3 -> A4 valuePattern을 만남.
+
+*/
 function Scanner(rawCTXFreeGrammar){
+    this.rootNode = null;
+    var temp = '';
+    var idPattern = new RegExp('[a-zA-Z_]', 'g');
+    var spacePattern = new RegExp('[(\n|\\s|\t)]', 'g');
+    var valuePattern = new RegExp('[a-zA-Z_0-9]', 'g');
+
     for(i in rawCTXFreeGrammar){
-        console.log(rawCTXFreeGrammar[i]);
+        if(rawCTXFreeGrammar[i].match(idPattern)){
+            
+        }else if(rawCTXFreeGrammar[i].match(spacePattern)){
+            console.log(i);
+        }
+        
     }
 }
 
